4ff2c74923ef5aeb67e65481bc2e538dc15e1c9b
Pragati Barse <108925468+pragatibarse@users.noreply.github.com>
2026-02-06 16:10:31 +0530

    Add files via upload

---
+++ C_Programming/Assignment_1/Assignment_1/vote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int age = 18;
+    if(age >= 18){
+        printf("person is eligible for vote");
+    }
+    else{
+        printf("person is not eligible for vote");
+    }
+}
