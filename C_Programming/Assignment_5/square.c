68fe0a8be4f1c7c2ff23da59c8dd4052eb781f58
Pragati Barse <108925468+pragatibarse@users.noreply.github.com>
2026-02-28 16:38:08 +0530

    Add files via upload

---
+++ C_Programming/Assignment_5/square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i,j;
+    
+    for(int i = 1; i <= 4; i++)
+    {
+        for(int j = 1; j <= 4;j++){
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+}
